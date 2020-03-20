ec4fdbfeb3ebfd30d66487ca56b8f7a7c4844e83
AkramYamin <ak.5@yahoo.com>
2020-03-20 18:17:06 +0200

    Created header.dart

---
+++ lib/widgets/utils/header.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Header extends AppBar {
+  static final logoImage = new Image.asset('images/logo.png');
+
+  static Widget getProfileHeader() {
+    return AppBar(
+      title: logoImage,
+      backgroundColor: Colors.transparent,
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.settings),
+          tooltip: 'Show Snackbar',
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  static Widget getCleanHeader() {
+    return AppBar(
+      title: logoImage,
+      backgroundColor: Colors.transparent,
+    );
+  }
+
+  static Widget getProfileSettingsHeader() {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios),
+        tooltip: 'Show Snackbar',
+        onPressed: () {},
+      ),
+      title: logoImage,
+      backgroundColor: Colors.transparent,
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.save),
+          tooltip: 'Show Snackbar',
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
