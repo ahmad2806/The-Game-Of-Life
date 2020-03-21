485a1c0e165f33d039cfde5e2a00dc9b8085f819
ahmad <ahmadhashem2806@gmail.com>
2020-03-21 03:40:35 +0200

    home page nad nav bar was created

---
+++ lib/screens/upper_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class UpperPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      // constraints: BoxConstraints.expand(),
+      // decoration: BoxDecoration(
+      //   color: Color.fromARGB(255, 255, 255, 255),
+      // ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/upper-back-ground.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "assets/images/star-icon.png",
+                fit: BoxFit.none,
+              ),
+            ],
+          ),
+          // Positioned(
+          //   top: 5,
+          //   child: Image.asset(
+          //     "assets/images/game-text.png",
+          //     fit: BoxFit.none,
+          //   ),
+          // ),
+          
+        ],
+      ),
+    );
+  }
+}
